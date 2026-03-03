8d66c108dc677d9c53b997c952f3d5340cbcee41
vzoli1987 <vzoli1987@gmail.com>
2026-03-03 18:44:33 +0100

    Repo update: 2026-03-03 18:44

--- plugin.video.filmhiradok/main.py
+++ plugin.video.filmhiradok/main.py
@@ -14,10 +14,9 @@ import os
 # --- KONFIGURÁCIÓ ---
 ADDON = xbmcaddon.Addon()
 BASE_URL = "https://filmhiradokonline.hu"
-# Felhasználói instrukció alapján a nagy bélyegkép alapja
 THUMB_URL_BASE = "https://filmhiradokonline.hu/keyframe/fo/"
 ITEMS_PER_PAGE = 20
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0'
 
 PROFILE_PATH = xbmcvfs.translatePath(ADDON.getAddonInfo('profile'))
 HISTORY_FILE = os.path.join(PROFILE_PATH, 'search_history.json')
@@ -28,8 +27,12 @@ session = requests.Session()
 # --- SEGÉDFÜGGVÉNYEK ---
 
 def get_html(url):
-    xbmc.sleep(500)
-    headers = {'User-Agent': USER_AGENT, 'Referer': BASE_URL}
+    xbmc.sleep(500) # 0.5s várakozás (felhasználói kérés)
+    headers = {
+        'User-Agent': USER_AGENT, 
+        'Referer': BASE_URL + '/index.php',
+        'Accept': 'application/json, text/plain, */*'
+    }
     try:
         r = session.get(url, headers=headers, timeout=10)
         r.encoding = 'utf-8'
@@ -50,40 +53,35 @@ def save_history(keyword):
     with open(HISTORY_FILE, 'w', encoding='utf-8') as f: json.dump(h[:10], f)
 
 def get_large_thumb(mvh_id):
-    """
-    User instruction: mvh_id-ból (pl. MFH_1989_23-04) nagy felbontású bélyegkép URL generálása.
-    URL formátum: https://filmhiradokonline.hu/keyframe/fo/MFH_1989_23-04.jpg
-    """
     if not mvh_id: return ""
     return "{}{}.jpg".format(THUMB_URL_BASE, mvh_id)
 
 # --- LISTÁZÁS ---
 
 def list_films_json(url, page=0):
-    html = get_html(url)
-    if not html: return
-    match = re.search(r'var films\s*=\s*(\[.*?\]);', html, re.DOTALL)
-    if not match: return
+    """Archívum és Évszámok (JSON alapú)."""
+    data_text = get_html(url)
+    if not data_text: return
     
-    try:
+    # Ha HTML-be ágyazott JSON (Archívum főoldal)
+    if "var films =" in data_text:
+        match = re.search(r'var films\s*=\s*(\[.*?\]);', data_text, re.DOTALL)
+        if not match: return
         all_films = json.loads(match.group(1))
-    except: return
+    else:
+        # Ha tiszta JSON (timemachine2.php)
+        try:
+            all_films = json.loads(data_text)
+        except: return
 
     start, end = page * ITEMS_PER_PAGE, (page + 1) * ITEMS_PER_PAGE
-
     for film in all_films[start:end]:
         f_id = film.get('id')
-        year = film.get('year', '')
-        title = film.get('title', '')
-        display_title = "({}) {}".format(year, title)
-        
-        # Nagy felbontású bélyegkép generálása az mvh_id alapján
+        display_title = "({}) {}".format(film.get('year', ''), film.get('title', ''))
         large_thumb = get_large_thumb(film.get('mvh_id'))
-        
         plot = "[B][COLOR green]ÖSSZEFOGLALÓ:[/COLOR][/B]\n" + film.get('content', '')
         if film.get('annotation'):
             plot += "\n\n[B][COLOR white]RÉSZLETEK:[/COLOR][/B]\n" + film.get('annotation', '')
-
         add_video_item(display_title, f_id, large_thumb, plot)
         
     if len(all_films) > end:
@@ -91,17 +89,15 @@ def list_films_json(url, page=0):
     finalize_directory('movies')
 
 def list_films_html(url):
+    """Kulcsszavas keresés és Legfrissebbek (HTML alapú)."""
     html = get_html(url)
     if not html: return
-    # HTML-ből kinyerjük awatch.php linkeket és a címet
-    items = re.findall(r'class="search_item".*?src="(.*?)".*?href="watch\.php\?id=(\d+)">(.*?)</a>', html, re.DOTALL)
     
+    items = re.findall(r'class="search_item".*?src="(.*?)".*?href="watch\.php\?id=(\d+)">(.*?)</a>', html, re.DOTALL)
     for thumb_part, db_id, title in items:
-        # A HTML-ben a kis kép URL-je van, de a getimage.php hívásból kinyerhetjük az mvh_id-t a nagy képhez
         mvh_id_match = re.search(r'src=([^&]+)', thumb_part)
         mvh_id = mvh_id_match.group(1) if mvh_id_match else ""
         large_thumb = get_large_thumb(mvh_id)
-        
         add_video_item(title.strip(), db_id, large_thumb, "")
     
     if 'class="right"' in html or ">next<" in html.lower():
@@ -112,13 +108,10 @@ def list_films_html(url):
 
 def add_video_item(title, db_id, thumb, plot):
     li = xbmcgui.ListItem(title)
-    li.setArt({'thumb': thumb, 'poster': thumb, 'icon': thumb, 'fanart': thumb}) # A bélyegképet használjuk fanartnak is a leírás mögé
-    
-    # Plot javítás Kodi 20/21-hez
+    li.setArt({'thumb': thumb, 'poster': thumb, 'icon': thumb, 'fanart': thumb})
     li.setInfo('video', {'plot': plot, 'title': title})
     li.getVideoInfoTag().setPlot(plot)
     li.getVideoInfoTag().setTitle(title)
-    
     li.setProperty('IsPlayable', 'true')
     u = "{}?mode=play&id={}".format(sys.argv[0], db_id)
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u, listitem=li, isFolder=False)
@@ -131,18 +124,17 @@ def finalize_directory(ctype):
     xbmcplugin.setContent(int(sys.argv[1]), ctype)
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
-# --- LEJÁTSZÁS (MONITORRAL ÉS SÜTI FIXEL) ---
+# --- LEJÁTSZÁS ---
 
 class FilmhiradoMonitor(xbmc.Player):
     def __init__(self, start_t, end_t):
-        self.start_t = float(start_t) if start_t else 0
-        self.end_t = float(end_t) if end_t else 0
+        self.start_t, self.end_t = float(start_t or 0), float(end_t or 0)
         self.seek_done = False
         xbmc.Player.__init__(self)
 
     def onAVStarted(self):
         if self.start_t > 0 and not self.seek_done:
-            xbmc.sleep(1500) 
+            xbmc.sleep(1500)
             self.seekTime(self.start_t)
             self.seek_done = True
 
@@ -157,45 +149,28 @@ def play_film(video_id):
     player_url = "{}/player.php?id={}".format(BASE_URL, video_id)
     html = get_html(player_url)
     if not html: return
-    
     video_match = re.search(r'<source\s+src=["\']([^"\']+\.mp4)["\']', html, re.I)
     if not video_match: return
-    
     video_url = video_match.group(1).replace('\\/', '/')
-    if not video_url.startswith('http'): 
-        video_url = "{}/{}".format(BASE_URL, video_url.lstrip('/'))
+    if not video_url.startswith('http'): video_url = "{}/{}".format(BASE_URL, video_url.lstrip('/'))
 
-    # JS változók kimentése az időzítéshez
     start_t = re.search(r'var\s+start\s*=\s*(\d+);', html).group(1) if "var start" in html else 0
     end_t = re.search(r'var\s+end\s*=\s*(\d+);', html).group(1) if "var end" in html else 0
 
-    # Sütik összeállítása a 403 hiba elkerülésére
     cookies = session.cookies.get_dict()
     cookie_str = "fo[cookieaccept]=extra"
-    if cookies:
-        cookie_str += "; " + "; ".join(["{}={}".format(k, v) for k, v in cookies.items()])
-
-    # Fejlécek a Kodi lejátszónak
-    headers = {
-        'User-Agent': USER_AGENT,
-        'Referer': player_url,
-        'Cookie': cookie_str
-    }
+    if cookies: cookie_str += "; " + "; ".join(["{}={}".format(k, v) for k, v in cookies.items()])
     
+    headers = {'User-Agent': USER_AGENT, 'Referer': player_url, 'Cookie': cookie_str}
     final_url = video_url + "|" + urllib.parse.urlencode(headers)
     
     li = xbmcgui.ListItem(path=final_url)
-    # Beállítjuk a bélyegképet a videóhoz is, ha elindul
-    mvh_id_match = re.search(r'id=(\d+)', player_url) # Ezt nehéz kinyerni az mvh_id-ből, de a player URL-ben ott az ID
-    large_thumb = "" # Nehéz kitalálni az mvh_id-t az ID-ból HTTP hívás nélkül
-    
     xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, listitem=li)
 
     monitor = FilmhiradoMonitor(start_t, end_t)
     for _ in range(40):
         if monitor.isPlaying(): break
         xbmc.sleep(500)
-        
     while monitor.isPlaying():
         if monitor.check_end(): break
         xbmc.sleep(1000)
@@ -208,8 +183,16 @@ def main():
     
     if not mode:
         add_dir("[B][COLOR red]:: LEGFRISSEBBEK ::[/COLOR][/B]", "{}/search.php?new".format(BASE_URL), mode='list_html')
-        add_dir("[B][COLOR green]:: ARCHÍVUM ::[/COLOR][/B]", "{}/index.php".format(BASE_URL), mode='list_json')
-        xbmcplugin.addDirectoryItem(int(sys.argv[1]), sys.argv[0]+"?mode=search_menu", xbmcgui.ListItem("[B][ KERESÉS ][/B]"), True)
+        add_dir("[B][COLOR green]:: ARCHÍVUM (Összes) ::[/COLOR][/B]", "{}/index.php".format(BASE_URL), mode='list_json')
+        xbmcplugin.addDirectoryItem(int(sys.argv[1]), sys.argv[0]+"?mode=year_menu", xbmcgui.ListItem("[B][ TALLÓZÁS ÉV SZERINT ][/B]"), True)
+        xbmcplugin.addDirectoryItem(int(sys.argv[1]), sys.argv[0]+"?mode=search_menu", xbmcgui.ListItem("[B][ KERESÉS SZÖVEGGEL ][/B]"), True)
+        finalize_directory('addons')
+        
+    elif mode == 'year_menu':
+        for year in range(1991, 1913, -1):
+            url = "{}/timemachine2.php?year={}".format(BASE_URL, year)
+            u = "{}?mode=list_json&url={}".format(sys.argv[0], urllib.parse.quote_plus(url))
+            xbmcplugin.addDirectoryItem(int(sys.argv[1]), u, xbmcgui.ListItem("[COLOR yellow]Évszám:[/COLOR] {}".format(year)), True)
         finalize_directory('addons')
         
     elif mode == 'search_menu':
